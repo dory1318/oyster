1417ddf83d6a18492758a541fb800290162ce9b0
Dora Kemeny <dory1318@gmail.com>
2019-02-11 15:17:46 +0000

    step 7 can top up and deduct oyster

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,21 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
 def initialize
   @balance = 0
 end
+
+def top_up(value)
+  fail 'Balance is over the limit' unless @balance + value <= LIMIT
+  @balance += value
+end
+
+def deduct(fare)
+  @balance -= fare
+end
+# def too_much?
+#   @balance + top_up(value) >= LIMIT
+# end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,19 @@ it 'has initial balance zero' do
   expect(subject.balance).to eq 0
 end
 
+it 'top up oystercard' do
+  subject.top_up(20)
+  expect(subject.balance).to eq 20
+end
+
+it 'balance cannot be more than £90' do
+  expect{subject.top_up(91)}.to raise_error 'Balance is over the limit'
+end
+
+it 'deducted money from oyster' do
+  subject.top_up(20)
+  subject.deduct(5)
+  expect(subject.balance).to eq 15
+end
+
 end
